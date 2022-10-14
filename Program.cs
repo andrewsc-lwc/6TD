c5079ef0378f422d38ae48dfbf0bf680bd57344e
andrewsc-lwc <64260666+andrewsc-lwc@users.noreply.github.com>
2022-10-14 07:59:37 +0100

    Starter code for Sixth Form Taster Day

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Animals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pet1Name = "Fido";
+            int pet1Age = 12;
+            int pet1Height = 55;
+            Console.WriteLine("My pet is called " + pet1Name + " and they are " + pet1Age + " years old.");
+
+
+            //string[,] myAnimals = { {"Fido","12","55" },{"Felix","9","43"} };
+            //Console.WriteLine("My pet is called " + myAnimals[0,0] + " and they are " + myAnimals[0,1] + " years old.");
+
+
+
+        }
+    }
+
+    //To comment a section select it, hold Control and K, and then Control and C.
+    //To uncomment a section select it, hold Control and K, and then Control and U.
+
+    //class Animal
+    //{
+    //    private string att1;
+    //    private string att2;
+    //    private string att3;
+
+    //    public Animal(string att1, string att2, string att3)
+    //    {
+    //        this.att1 = att1;
+    //        this.att2 = att2;
+    //        this.att3 = att3;
+    //    }
+
+    //    public string GetInfo()
+    //    {
+    //        string details = "temp";
+    //        return details;
+    //    }
+    //}
+}
